1dcd9c49513e057a099c2605373d61581e958f72
Preetham N <preetham.in.1997@gmail.com>
2016-12-23 13:51:56 -0800

    Shows grades in popup on sa.ucla.edu as well

--- saUclaEduMain.js
+++ saUclaEduMain.js
@@ -29,18 +29,22 @@ function listener()
 
 // 
 function findExistingClasses(){
-  var uclaClasses = document.getElementsByClassName('class-info primary-row');
+  var uclaClasses = document.getElementsByClassName('row-fluid class-title');
+  var subject = document.getElementById('spanSearchResultsHeader').textContent.match(/\(([^()]*)\)$/i)[1].replace(/\s/g, '-');
 
   for(var i=0;i<uclaClasses.length;i++){
     var instCont = uclaClasses[i].getElementsByClassName('instructorColumn');
+    var classNumber = uclaClasses[i].getElementsByClassName('head')[0].textContent;
+    classNumber = classNumber.substring(0, classNumber.indexOf(' '))
+    var className = subject + '-' + classNumber;
 
-    for(var j=0;j<instCont.length;j++){
+    for(var j=1;j<instCont.length;j++) { //start from 1 because the first instructorColumn isn't an actual instructor
       if(instCont[j].hasAttribute("buttons-added")){
         var a = "LUL";
       }
       else{
           instCont[j].setAttribute("buttons-added","true");
-          addInstButtons(instCont[j]);
+          addInstButtons(instCont[j], className);
       }
     }
   }
@@ -48,7 +52,7 @@ function findExistingClasses(){
 
 
 // Function for adding the button next to instructor names
-function addInstButtons(instCont){
+function addInstButtons(instCont, className){
   // console.log(uclaClassObj.innerHTML);
 
   if(instCont.getElementsByClassName('inst-button-bwalk').length>0) 
@@ -89,7 +93,7 @@ function addInstButtons(instCont){
   // this re-adds the professor names, each between paragraph tags
   for(var i=0;i<numInst;i++){
     var pCont = document.createElement("p")
-    $(pCont).append(instructorName[i].substring(0,15));
+    $(pCont).append(instructorName[i]);
     pCont.title = instructorName[i];
 
     // creates and adds the buttons!
@@ -100,7 +104,7 @@ function addInstButtons(instCont){
     if(instructorName[i]!="TA"){
 
       pCont.appendChild(bwalkButton);
-      getInstSearchRes(bwalkButton,instructorName[i]);
+      getInstSearchRes(bwalkButton, instructorName[i], className);
       //instCont.appendChild(pContForBut)
     }
     
@@ -111,7 +115,7 @@ function addInstButtons(instCont){
 
 // gets the search results page from bruin walk and
 // sets the overall rating score for the professor.
-function getInstSearchRes(bwalkButton,instructorName){
+function getInstSearchRes(bwalkButton, instructorName, className){
 
   var uriInstName = encodeURI(instructorName);
   bwalkButton.addEventListener("mouseout",instButtEvLisMO(bwalkButton));
@@ -162,14 +166,14 @@ function getInstSearchRes(bwalkButton,instructorName){
           if(score < 2) bwalkButton.className = "inst-button-bwalk bad";
           else if(score > 3.5) bwalkButton.className = "inst-button-bwalk good";
 
-          bwalkButton.addEventListener("mouseover",instButtEvLis(bwalkButton,true,tempDiv));
+          bwalkButton.addEventListener("mouseover",instButtEvLis(bwalkButton,true,instNameFromRes,className));
         }
         // if it doesn't match
         else{
           bwalkButton.innerHTML = "N/A";
           bwalkButton.setAttribute("found-tag","NOT_FOUND");
 
-          bwalkButton.addEventListener("mouseover",instButtEvLis(bwalkButton,false,tempDiv));
+          bwalkButton.addEventListener("mouseover",instButtEvLis(bwalkButton,false,instNameFromRes,className));
         }
       }
 
@@ -186,15 +190,14 @@ function getInstSearchRes(bwalkButton,instructorName){
 // this function is whats called when the button is mouseovered
 // This adds the popup thingy when the score button is mouseovered
 // so that more details ratings can be seen SeemsGood
-function instButtEvLis(instBut,found,resultDOM) {
+function instButtEvLis(instBut,found,instNameFromRes,className) {
   return function(){
     // If popover hasn't been loaded, load it
     var popupContainers = instBut.parentElement.getElementsByClassName("inst-rating-popup-cont");
     if(popupContainers.length==0){
       // if professor was found
       if(found){
-        var seeMoreLink = resultDOM.getElementsByClassName("sr-info")[0].getElementsByClassName("see-more")[0];
-        var instPageUrl = "http://www.bruinwalk.com" + seeMoreLink.href.substring(seeMoreLink.href.indexOf("/professors")) +"all";
+        var instPageUrl = "http://www.bruinwalk.com/professors/" + instNameFromRes.replace(/\s/g, '-').toLowerCase() +"/"+ className.toLowerCase() +"/";
         instBut.href = instPageUrl;
         instBut.target = "_blank";
         //console.log(instPageUrl);
@@ -221,6 +224,10 @@ function instButtEvLis(instBut,found,resultDOM) {
 
               var popup = document.createElement("div");
               popup.className = "inst-rating-popup popover-content";
+              popup.style.width = "16em";
+              console.log(popup.style.paddingRight);
+              popup.style.paddingRight = "4em";
+              console.log(popup.style.paddingRight);
               //popup.textContent = "Showing ";
 
               popup.append("Showing Bruin Walk results for: " + instBut.getAttribute("found-tag"));
@@ -245,11 +252,52 @@ function instButtEvLis(instBut,found,resultDOM) {
                 //console.log(value);
               }
 
-              if(ratingRows.length==0)
-                table.append("This instructor has no recorded ratings");
+              if(ratingRows.length==0) {
+                instBut.href = "http://www.bruinwalk.com/professors/" + instNameFromRes.replace(/\s/g, '-').toLowerCase() +"/all/";
+                table.append("This instructor has no recorded ratings for this class (click to see ratings for other classes)");
+              }
               
               table.style.marginTop = "10px";
               popup.appendChild(table);
+
+              var allScores = tempDiv.getElementsByClassName('graph-body');
+              var gradesTable = document.createElement('table');
+              if(allScores.length > 0) {
+                var scores = allScores[0].getElementsByClassName('bar-fill has-tip tip-left');
+                var grades = ['A+', 'A', 'A-', 'B+', 'B', 'B-', 'C+', 'C', 'C-', 'D+', 'D', 'D-', 'F'];
+                var gradeGoodThresholds = [50, 40, -20, -10, -10]; //array of good scores for each letter grade (negative means less is better)
+                var gradeBadThresholds = [-15, -20, 30, 20, 15]; //array of good scores for each letter grade (negative means less is better)
+
+                var row;
+                var sum = 0;
+                for(var i=0; i<scores.length; i++) {
+                    var value = scores[i].getAttribute('title');
+
+                    if(i%3 == 0) row = document.createElement("tr");
+                    var td1 = document.createElement("td");
+                    var td2 = document.createElement("td");
+                    td1.append(grades[i]);
+                    td2.append(value);
+                    sum += parseFloat(value.substring(0, value.indexOf('%')));
+                    row.appendChild(td1);
+                    row.appendChild(td2);
+                    if(i%3 == 2 || i==scores.length-1) {
+                      var j = Math.floor(i/3);
+                      if((gradeGoodThresholds[j] > 0 && sum > gradeGoodThresholds[j]) || (gradeGoodThresholds[j] < 0 && sum < -gradeGoodThresholds[j])) row.style.background = '#00FF00';
+                      else if((gradeBadThresholds[j] > 0 && sum > gradeBadThresholds[j]) || (gradeBadThresholds[j] < 0 && sum < -gradeBadThresholds[j])) row.style.background = '#FF0000';
+                      gradesTable.appendChild(row);
+                      sum = 0;
+                    }
+                }
+              } 
+              // the class doesn't have grades
+              else {
+                gradesTable.append("This instructor has no recorded grades for this class");
+              }
+
+              gradesTable.style.marginTop = "10px";
+              popup.appendChild(gradesTable);
+
 
               var footer = document.createElement("p");
               footer.append("Click to see full Bruin Walk page");
@@ -265,13 +313,15 @@ function instButtEvLis(instBut,found,resultDOM) {
 
       //   if professor wasnt found at bruinwalk
       else{
+        instBut.href = "http://www.bruinwalk.com/professors/" + instNameFromRes.replace(/\s/g, '-').toLowerCase() +"/all/";
         // creates pop-up container
         var popupCont = document.createElement("div");
         popupCont.className = "inst-rating-popup-cont show popover clickover fade bottom in";
 
         var popup = document.createElement("div");
         popup.className = "inst-rating-popup popover-content inst-nonexistent";
-        popup.textContent = "This professor cannot be found at ";
+        popup.textContent = instNameFromRes + " cannot be found for this class on ";
+        popup.style.width = "16em";
 
         var bruinWalkLink = document.createElement("a");
         bruinWalkLink.href = "http://www.bruinwalk.com";
